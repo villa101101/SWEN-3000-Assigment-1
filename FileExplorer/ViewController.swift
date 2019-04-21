c4a719c6b887b9e6ee428efad4047aec8eb7979f
KJ <kernel1983@gmail.com>
2019-04-21 22:28:25 +0800

    Initial Commit

---
+++ FileExplorer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FileExplorer
+//
+//  Created by kj on 2019/4/21.
+//  Copyright © 2019 GIC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
