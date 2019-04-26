dc3bf0408d109958bd66a30c5e93875707fd4961
David Thomas <david.thomas10111@yahoo.com>
2019-04-26 08:36:25 +0800

    added two buttons and link them to creating and listing directories for trial

--- FileExplorer/ViewController.swift
+++ FileExplorer/ViewController.swift
@@ -13,6 +13,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+    }
+    
+    @IBAction func buttonHandlerCreateDirectory(_ sender: Any) {
+        //create directory
+        let documentsPath = NSURL(fileURLWithPath: NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0])
+        let logsPath = documentsPath.appendingPathComponent("data")
+        print(logsPath!)
+        
+        do{
+            try FileManager.default.createDirectory(atPath: logsPath!.path, withIntermediateDirectories: true, attributes: nil)
+            
+        }catch let error as NSError{
+            print("Unable to create directory",error)
+        }
+        
+        
+    }
+    
+    @IBAction func buttonHandlerListing(_ sender: Any) {
+        let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+        let url = NSURL(fileURLWithPath: path)
+        let filePath = url.path
+        let fileManager = FileManager.default
+        print(try! fileManager.contentsOfDirectory(atPath: filePath!))
     }
 
 
