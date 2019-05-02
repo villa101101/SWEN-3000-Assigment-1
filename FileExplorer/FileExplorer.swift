e54332d521d484e3856f0f28c8c84b7d78e4659c
David Thomas <david.thomas10111@yahoo.com>
2019-05-02 14:43:56 +0800

    added three folders to the server

---
+++ FileExplorer/FileExplorer.swift
@@ -0,0 +1,15 @@
+//
+//  FileExplorer.swift
+//  FileExplorer
+//
+//  Created by administritor on 2019/4/26.
+//  Copyright © 2019 GIC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class FileExplorer : UIDocumentBrowserViewController, UIDocumentBrowserViewControllerDelegate {
+    
+}
--- FileExplorer/ViewController.swift
+++ FileExplorer/ViewController.swift
@@ -7,19 +7,38 @@
 //
 
 import UIKit
+import Foundation
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
     
+//    @IBOutlet weak var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        webView.uiDelegate = self
+//        webView.navigationDelegate = self
+//
+//        let url = Bundle.main.url(forResource: "http://127.0.0.1:8000/?folder", withExtension: "html")!
+//        webView.loadFileURL(url, allowingReadAccessTo: url)
+//        let request = URLRequest(url: url)
+//        webView.load(request)
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        
+        do {
+            let items = try fm.contentsOfDirectory(atPath: path)
+            
+            for item in items {
+                print("Found \(item)")
+            }
+        } catch {
+            // failed to read directory – bad permissions, perhaps?
         }
-    
-    
-    
 
 
-
+}
 }
 
